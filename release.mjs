e0881add0f049bccdaa10bfbf4410f35d632cb0d
Wesley Stam <wstam88@gmail.com>
2022-01-26 12:40:07 +0100

    Updates...

--- release.mjs
+++ release.mjs
@@ -1,43 +1,111 @@
 #!/usr/bin/env zx
+/* eslint-disable */
 import 'zx/globals'
 import inquirer from 'inquirer'
+import semverInc from 'semver/functions/inc.js'
+import { exit } from 'process'
+const packageInfo = require('./package.json')
 
-// const packageJson = JSON.parse(await $`cat ../package.json`)
-const answers = await inquirer.prompt([
-  {
-    type: 'list',
-    name: 'next_branch',
-    message: 'Which branch do you want to release?',
-    choices: ['master', 'next', 'next-dev', 'next-feature', 'next-hotfix', 'next-release'],
-  },
+/**
+ * Set options for zx.
+ */
+$.verbose = false
+
+/**
+ * Package.json information
+ */
+const { release_type } = await inquirer.prompt([
   {
     type: 'list',
     name: 'release_type',
     message: 'Which release type?',
-    choices: ['patch', 'minor', 'major'],
+    choices: ['patch', 'minor'],
+    required: true,
   },
 ])
 
-let fix_version = ''
-let next_version = ''
-let next_branch = ''
-let current_branch = ''
+await $`git fetch --quiet --tags origin`
+
+const revList = await $`git rev-list --tags --max-count=1`
+const lastReleasedVersion = (
+  await $`git describe --tags ${revList} | sed -r 's/Release-//g'`
+).stdout.trim()
+const currentPackageVersion = packageInfo.version
+
+const currentRelease = `Release-${lastReleasedVersion}`
+const currentBranch = (await $`git rev-parse --abbrev-ref HEAD`).stdout.trim()
+const nextVersion = semverInc(lastReleasedVersion, release_type)
+const tagName = `Release-${nextVersion}`
+const tagMessage = `FEA-${nextVersion}`
+
+const log = console.log
+
+
+log(currentBranch)
+
+/**
+ * We should be on the master branch
+ * ...of course we could switch to the master branch automaticlly?
+ */
+if (currentBranch !== 'master') {
+  log('You are not on the master branch. Please switch to the master branch before continuing.')
+    exit(1)
+}
+
+/**
+ * There should be no uncommitted changes.
+ */
+if (await $`git status -s`) {
+  log('There are uncommitted changes. Please commit or stash them before continuing.')
+    exit(1)
+}
+
+/**
+ * There should be no uncommitted changes.
+ */
+if (await 'git diff FETCH_HEAD') {
+  log('Local repository contains unpushed commits, abort the release.')
+    exit(1)
+}
+
+log(`Current version: ${currentRelease}`)
+log(`Tag name: ${tagName}`)
+log(`Tag message: ${tagMessage}\n`)
 
 /**
  * Make a release of the project.
  */
-// function makeRelease() {
-//   //   console.log(`Making release ${packageJson.version}`)
-//   if (await $`git checkout ${next_branch}`) {
-//     console.log(`Switched to ${next_branch}`)
-//   } else {
-//     await $`git checkout master`
-//     await $`git pull origin master`
-//     await $`git checkout -B ${$next_branch}`
-//     await $`git push -u origin ${next_branch}`
-//   }
+async function makeRelease() {
+  await $`git checkout master`
+  await $`git pull origin master`
 
-//   const tag_on_current_commit = await $`git tag -l --points-at HEAD`
-// }
+  setPackageVersion(nextVersion)
 
-// makeRelease()
+  await $`git add package.json`
+  await $`git commit -m "${tagName}"`
+
+  const tagOnCurrentCommit = (await $`git tag -l --points-at HEAD`).stdout
+
+  if (tagOnCurrentCommit) {
+    log('There is already a tag on this commit. Skipping...')
+    exit(1)
+  } else {
+    // Add git tag
+    log(`Add git tag ${tagName} with message: ${tagMessage}`)
+    await $`git tag -a ${tagName} -m "${tagMessage}"`
+
+    // Push git tag
+    log(`Push the ${tagName} tag to origin`)
+    await $`git push origin master`
+  }
+}
+
+/**
+ * Update the package.json version.
+ */
+function setPackageVersion(version) {
+  packageInfo.version = version
+  fs.writeFileSync('./package.json', JSON.stringify(packageInfo, null, 2))
+}
+
+makeRelease()
